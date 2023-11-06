f3fc98c8f20c28b9b2109a12d180469d7c637dde
Nissim Amsallem <nissimamsallem@gmail.com>
2023-11-06 15:58:50 +0200

    build templet for chat

---
+++ src/components/Chat.jsx
@@ -0,0 +1,33 @@
+import Box from '@mui/material/Box';
+import { Button, Container } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+
+export default function Chat() {
+  return (<div>
+            <Box sx={{ flexGrow: 1,height:'90vh'}}>
+                <Container >
+                    <Grid2 container spacing={0}>
+                        <Grid2 xs={3}>
+                            <Box  sx={{ bgcolor: '#DDDDDD', height: '90vh' }}>
+                            </Box>
+                        </Grid2>
+
+                        <Grid2 xs={9}>
+                            <Box  sx={{ bgcolor: '#AAAAAA', height: '10vh' }}>
+                            </Box>
+
+                            <Box  sx={{ bgcolor: '#BBBBBB', height: '60vh' }}>
+                            </Box>
+
+                            <Box  sx={{ bgcolor: '#CCCCCC', height: '20vh' }}>
+                                <Button>Send</Button>
+                            </Box>
+
+                        </Grid2>
+                    </Grid2>
+                </Container>
+            </Box>
+        </div>
+    );
+}
+    
