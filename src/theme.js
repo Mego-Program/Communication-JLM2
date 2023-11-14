a204b4dcf7bc62a3cb059e7ca699173fcbac3032
reuvencf <rcerfabian@gmail.com>
2023-11-14 11:36:47 +0200

    1.migratesome elements to mui 2.adding theme colors.

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-import { red} from '@mui/material/colors';
+import { red } from '@mui/material/colors';
 
 
 // Create a theme instance.
@@ -8,9 +8,23 @@ const theme = createTheme({
     primary: {
       main: '#F6C927',
     },
+    
     secondary: {
       main: '#31213E',
     },
+    
+    chat: {
+      main: '#21213E',
+      navBar:'#0A0A1B',
+
+    },
+    
+    white: {
+      main: '#ffffff',
+
+    },
+
+
     error: {
       main: red.A400,
     },
