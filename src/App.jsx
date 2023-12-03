85ac58dfb259a90c69821fc6a5f5f6c5617fc25a
reuvencf <rcerfabian@gmail.com>
2023-12-03 16:57:20 +0200

    added configs for connecting with infra moved theme to app for preventing duplicate rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 
 import Chat from "./components/Chat";
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from "./components/theme";
+
 
 export default  function App() {
   return (
     <div>
-  <Chat/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Chat/>
+    
+    </ThemeProvider>
+  
     </div>
   )
     }
