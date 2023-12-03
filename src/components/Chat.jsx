e8373ed86bf000fc025ddc742e140af9defbf788
reuvencf <rcerfabian@gmail.com>
2023-12-03 11:49:31 +0200

    separting the entry room from the chat adding small changes

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,69 +1,121 @@
-import io from 'socket.io-client';
-import {useEffect, useState} from "react";
-import  Button from "@mui/material/Button";
-import { Container, Input, TextField,Box } from '@mui/material';
-import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
-import theme from './theme';
-import ChatBody from './chat-body';
+import io from "socket.io-client";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import { Container, Input, TextField, Box } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import theme from "./theme";
+import ChatBody from "./chat-body";
 
-//const socket = io.connect("https://jlm-com-server-2.onrender.com/")
-const socket = io.connect("http://localhost:3001")
-const ariaLabel = { 'aria-label': 'description' };
-
+const socket = io.connect("https://jlm-com-server-2.onrender.com/");
+// const socket = io.connect("http://localhost:3001")
+const ariaLabel = { "aria-label": "description" };
 
 export default function Chat() {
-    const [room, setRoom] = useState('')
-    const [username, setUsername] = useState('')
-    const [roomList, setRoomList] = useState([<Button>{"Main Room"}</Button>])
 
+  const cosntUser = "nissim"
+  const [room, setRoom] = useState("");
+  const [username, setUsername] = useState("");
+  const [roomList, setRoomList] = useState([<Button>{"Main Room"}</Button>]);
+  const [showChat, setshowChat] = useState(false);
+  //const [showrooms, setshowrooms] = useState(false)
+
+  const joinRoom = (Event) => {
     
-    const joinRoom = (Event) => {
-        if(room!==''){
-            socket.emit("join_room", room)
-            setRoomList([...roomList,<Button>{room}</Button>])
-            console.log(room)
-            Event.target.value =""        }
-        else{
-            console.log("name room is empty")
-        }
-    };
+    console.log(roomList, room, roomList.includes(room));
 
+    if (room.trim(" ") && username.trim(" ")) {
+      if (!roomList.includes(room)) {
+        socket.emit("join_room", room);
+        setRoomList([...roomList, room]);
+      }
+      Event.target.value = "";
+      setshowChat(true);
+    } else {
+      console.log("name room is empty");
+    }
+  };
 
-  return (<div>
-            <Box bgcolor={theme.palette.chat.main} flexGrow= {1} height={'100vh'}>
-                <Container >
-                    <Grid2 container spacing={1}>
-                        <Grid2 xs={2}>
-                        <Input sx={{input: { color: '#ffffff' }, borderRadius:"7px", margin:'20px 0px 7px', width: "100%" , border: "2px #Ffffff solid",bgcolor: "#21213E"}} placeholder="user name" inputProps={ariaLabel}
-                                onKeyDown={(Event)=>{if(Event.key==="Enter"){setUsername(Event.target.value)}}}
-                                />
-                            <Box marginTop={'2px'} border= {"2px #F6C927 dotted"} borderRadius={'7px'} bgcolor= {theme.palette.chat.navBar} height= {'85vh'}>
-                                <Button bgcolor={theme.palette.white.main} onClick={joinRoom}>join room</Button>
-                                <TextField
-                                sx={{ input: { color: '#ffffff' } }}
-                                    type="text"
-                                    name='room number'
-                                    placeholder = "room number"
-                                    onChange={(Event) => {
-                                        setRoom(Event.target.value)
-                                    }} 
+  return (
+    <div>
+      <Box bgcolor={theme.palette.chat.main} flexGrow={1} height={"100vh"}>
+        {!showChat ? (
+          <Container>
+            <Grid2
+              height={"100vh"}
+              display={"flex"}
+              flexDirection={"column"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Grid2>
+                <Input
+                  sx={{
+                    input: { color: "#ffffff" },
+                    borderRadius: "7px",
+                    margin: "20px 0px 7px",
+                    width: "100%",
+                    border: "2px #Ffffff solid",
+                    bgcolor: "#21213E",
+                    padding:"0 0 0 100px "
+                  }}
+                  disableUnderline={true}
+                  placeholder="user name"
+                  inputProps={ariaLabel}
+                  onChange={(Event) => setUsername(Event.target.value)}
+                />
 
-                                    onKeyDown={(Event)=>{if(Event.key==="Enter"){joinRoom(Event)}}}
-                                />
-                                 <Box>
-                                   {roomList}
-                                </Box>
-                            </Box>
-                        </Grid2>
-
-                        <Grid2 marginTop={"11px"}xs={10}>
-                        <ChatBody socket={socket} username={username} room={room}/>
-                        </Grid2>
-                        
-                    </Grid2>
-                </Container>
-            </Box>
-        </div>
-    );
+                <Box
+                  marginTop={"2px"}
+                  border={"2px #F6C927 solid"}
+                  borderRadius={"7px"}
+                  bgcolor={theme.palette.chat.navBar}
+                  height={"50vh"}
+                  display={'flex'}
+                  flexDirection={'column'}
+                  justifyContent={'center'}
+                  alignItems={'center'}
+                >
+                  <TextField
+                    sx={{ input: { color: "#ffffff" }, width:"20vh"}}
+                    type="text"
+                    name="room number"
+                    placeholder="room number"
+                    onChange={(Event) => {
+                      setRoom(Event.target.value);
+                    }}
+                    onKeyDown={(Event) => {
+                      if (Event.key === "Enter") {
+                        joinRoom(Event);
+                      }
+                    }}
+                  />
+                    <Button bgcolor={theme.palette.white.main} onClick={joinRoom}>
+                        join room
+                    </Button>
+                  {/* <Box className="room_list">{roomList}</Box> */}
+                </Box>
+              </Grid2>
+            </Grid2>
+          </Container>
+        ) : (
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            xs={12}
+          >
+            <Button
+              sx={{ width: "4vh",marginTop:'20px' }}
+              onClick={() => {
+                setshowChat(false) && setRoom("");
+              }}
+            >
+              Home page
+            </Button>
+             <ChatBody socket={socket} username={username} room={room} />          
+          </Box>
+        )}
+      </Box>
+    </div>
+  );
 }
-    
--- src/components/chat-body.jsx
+++ src/components/chat-body.jsx
@@ -1,114 +1,220 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { Input, Button, Box, TextField, Stack } from '@mui/material'
-import { Send } from '@mui/icons-material';
-import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
-import theme from './theme';
-import { animateScroll } from 'react-scroll';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Input, Button, Box, TextField, Stack } from "@mui/material";
+import { Send } from "@mui/icons-material";
+import theme from "./theme";
+import { animateScroll } from "react-scroll";
 
 function scrollToBottom() {
   animateScroll.scrollToBottom({
-    containerId: "chat-box"
+    containerId: "chat-box",
   });
 }
 
-const ariaLabel = { 'aria-label': 'description' };
+const ariaLabel = { "aria-label": "description" };
 
-export default function ChatBody({socket, username, room}) {
-  const [message, setMessage] = useState ("");
-  const [messageReceived, setMessageReceived] = useState([])
-  const [scrollFlag, setscrollFlag] = useState(false)
+export default function ChatBody({ socket, username, room }) {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState([]);
+  const [scrollFlag, setscrollFlag] = useState(false);
 
   const sendMessage = async () => {
-    if (message!=='' && username!==''){
-        // console.log(message)
-        console.log(messageReceived);
-        const messageData = {
-            room: room,
-            aouter: username,
-            message: message,
-            time: 
-            new Date(Date.now()).getHours() + ':' +
-            new Date(Date.now()).getMinutes(), 
-        } 
-        await socket.emit('send_message', messageData)
-        setscrollFlag(true)
-        setMessageReceived((list) => [...list,messageData]);
-        setMessage('')
-        } 
+    if (message.trim(" ") && username.trim(" ") ) {
+      console.log(messageReceived);
+      const messageData = {
+        room: room,
+        aouter: username,
+        message: message,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData);
+      setscrollFlag(true);
+      setMessageReceived((list) => [...list, messageData]);
+      setMessage("");
     }
-  
-    useEffect(() => {
-      socket.on("receive_message",(message) => {
-        setMessageReceived((list) => [...list,message]);
-      });
-      return () => socket.off("receive_message")
-    },[socket]);
+  };
 
-   useEffect( () => {
-    if (scrollFlag){
-      scrollToBottom()
-      setscrollFlag(false)
-    } })
+  useEffect(() => {
+    socket.on("receive_message", (message) => {
+      setMessageReceived((list) => [...list, message]);
+    });
+
+    socket.on("privte_message", (message) => {
+      setMessageReceived((list) => [...list, message]);
+    });
+
+    return () => {
+      socket.off("receive_message");
+      socket.off("privte_message");
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    if (scrollFlag) {
+      scrollToBottom();
+      setscrollFlag(false);
+    }
+  });
   return (
     <div>
-     
-      <div className='chat-body'>
-      <Grid2 >
-          <Box display="flex" justifyContent="center" alignItems={"center"} className='chat-header' border= {"2px #F6C927 solid"} borderRadius={'7px'} margin={'7px'} color={theme.palette.primary.main} bgcolor={ theme.palette.chat.navBar} height= {'10vh'} >
+      <Box width="60vh" display={"flex"} flexDirection={"column"}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems={"center"}
+          className="chat-header"
+          border={"2px #F6C927 solid"}
+          borderRadius={"7px"}
+          margin={"7px"}
+          color={theme.palette.primary.main}
+          bgcolor={theme.palette.chat.navBar}
+          height={"10vh"}
+        >
           {`${username} You are on live chat in room: ${room}`}
-            
-          </Box>
+        </Box>
 
-          <Box id="chat-box" sx={{color: "#ffffff" ,overflow: "auto",
-          overflowY: "scroll" }}border= {"2px #F6C927 solid"} borderRadius={'7px'} margin={'7px'} bgcolor= {theme.palette.white} height= {'60vh' }
-          >
-              {messageReceived.map((message_content) => {
-                if (message_content.aouter === username) {
-                return (
-                  <Box display='flex' justifyContent="right" margin={"10px 10px 10px 70px"}>
-                  <Stack border={"2px gray solid"} borderRadius={"10px 20px 0px 20px "}>
-                  <Box display={"flex"} flexDirection={"row"} justifyContent={"space-between"} fontSize='10px' padding='3px 15px 3px 3px' sx={{bgcolor: "blue", width:"100%"}} borderBottom={"2px gray solid"} borderRadius={"8px 17px 0px 0px"}>
-                      <Box padding={"0px 25px 0px 0px"}>{message_content.aouter}</Box>
-                      <Box bgcolor={"white"}>{message_content.time}</Box> 
-                  </Box>
-                    <Box id="content" padding="2px 10px" color={"#000000"} bgcolor={"#ffffff"} borderRadius={"0px 0px 0px 18px"}>
-                      {message_content.message}</Box>
-                    </Stack>
-                </Box>)}
-                else {
-                  return (
-                  
-                    <Box display='flex'  margin={"10px 70px 10px 10px"}>
-                  <Stack border={"2px gray solid"} borderRadius={"15px 7px 15px 0px"}>
-                  <Box display={"flex"} flexDirection={"row"} justifyContent={"space-between"}fontSize='10px' padding='5px' sx={{bgcolor: "purple", width:"100%", borderRadius:"13px 5px 0px 0px"}} borderBottom={"2px gray solid"}>
-                      <Box padding={"0px 25px 0px 0px"}>{message_content.aouter}</Box>
-                      <Box >{message_content.time}</Box> 
-                  </Box>
-                    <Box padding={"0px 5px 0px 10px"} display={"flex"} color={"black"} bgcolor={"#ffffff"} borderRadius={"0px 0px 13px 0px"}>
-                      {message_content.message}</Box>
-                    </Stack>
+        <Box
+          id="chat-box"
+          sx={{ color: "#ffffff", overflow: "auto",  "&::-webkit-scrollbar":{
+            width:"10px"
+          },"&::-webkit-scrollbar-track":{
+            boxShadow: 'inset 0 0 5px silver;'
+          },'&::-webkit-scrollbar-thumb':{
+            background: "gold",
+            borderRadius: "10px"  },
+            "&::-webkit-scrollbar-thumb:hover": {
+              background: "#b30000" 
+            }
+          }}
+          border={"2px #F6C927 solid"}
+          borderRadius={"7px"}
+          margin={"7px"}
+          bgcolor={theme.palette.white}
+          height={"55vh"}
+        >
+          {messageReceived.map((message_content,index) => {
+            if (message_content.aouter === username) {
+              return (
+                <Box
+                  key={index}
+                  display="flex"
+                  justifyContent="right"
+                  margin={"10px 10px 10px 70px"}
+                >
+                  <Stack
+                    border={"2px gray solid"}
+                    borderRadius={"10px 20px 0px 20px "}
+                  >
+                    <Box
+                      display={"flex"}
+                      flexDirection={"row"}
+                      justifyContent={"space-between"}
+                      fontSize="10px"
+                      padding="3px 15px 3px 3px"
+                      sx={{ bgcolor: "blue", width: "100%" }}
+                      borderBottom={"2px gray solid"}
+                      borderRadius={"8px 17px 0px 0px"}
+                    >
+                      <Box padding={"0px 25px 0px 0px"}>{"Me"}</Box>
+                      <Box bgcolor={"white"}>{message_content.time}</Box>
                     </Box>
-                  
-                    )}  
-                  }
-              )}
+                    <Box
+                      id="content"
+                      padding="2px 10px"
+                      color={"#000000"}
+                      bgcolor={"#ffffff"}
+                      borderRadius={"0px 0px 0px 18px"}
+                    >
+                      {message_content.message}
+                    </Box>
+                  </Stack>
+                </Box>
+              );
+            } else {
+              return (
+                <Box display="flex" margin={"10px 70px 10px 10px"}>
+                  <Stack
+                    border={"2px gray solid"}
+                    borderRadius={"15px 7px 15px 0px"}
+                  >
+                    <Box
+                      display={"flex"}
+                      flexDirection={"row"}
+                      justifyContent={"space-between"}
+                      fontSize="10px"
+                      padding="5px"
+                      sx={{
+                        bgcolor: "purple",
+                        width: "100%",
+                        borderRadius: "13px 5px 0px 0px",
+                      }}
+                      borderBottom={"2px gray solid"}
+                    >
+                      <Box padding={"0px 25px 0px 0px"}>
+                        {message_content.aouter}
+                      </Box>
+                      <Box>{message_content.time}</Box>
+                    </Box>
+                    <Box
+                      padding={"0px 5px 0px 10px"}
+                      display={"flex"}
+                      color={"black"}
+                      bgcolor={"#ffffff"}
+                      borderRadius={"0px 0px 13px 0px"}
+                    >
+                      {message_content.message}
+                    </Box>
+                  </Stack>
+                </Box>
+              );
+            }
+          })}
+        </Box>
+
+        <Box
+          display={'flex'}
+          flexDirection={'row'}
+          border={"2px #F6C927 solid"}
+          borderRadius={"7px"}
+          margin={"7px"}
+          bgcolor={theme.palette.chat.navBar}
+          height={"10vh"}
+        >
+          <Input
+            multiline={true}
+            sx={{
+              overflow:'auto',
+              borderRadius: "7px",
+              padding:'0 5px 0 20px',
+              margin:'1px',
+              width: "80%",
+              "&::-webkit-scrollbar":{
+              width:"5px"}
+            }}
+            disableUnderline={true}
+            placeholder="message"
+            inputProps={{ style:{ color: "#ffffff" }}}
+            value={message}
+            onChange={(Event) => {
+              setMessage(Event.target.value);
+            }}
+            onKeyDown={(Event) => {
+              if (Event.key === "Enter") {
+                Event.preventDefault()
+                sendMessage(Event);
+              }
+            }}
+          />
+          <Button
+            sx={{ color: "#ffffff"}}
+            onClick={sendMessage}
+            endIcon={<Send />}
+          />
           </Box>
-
-          <Box border= {"2px #F6C927 solid"} borderRadius={'7px'} margin={'7px'} bgcolor={ theme.palette.chat.navBar} height= {'20vh' }>
-            
-          <Input sx={{ input: { color: '#ffffff' }, borderRadius:"7px" ,margin: '5px' , width: "98%" , border: "2px #Ffffff dotted"}} placeholder="message" inputProps={ariaLabel}
-                value={message}
-               onChange={(Event) => {
-                  setMessage(Event.target.value)
-                  }}
-                  onKeyDown={(Event)=>{if(Event.key==="Enter"){sendMessage(Event)}}}
-                  />
-                  <Button sx={{color: '#ffffff'}} onClick={sendMessage} endIcon={<Send/>} >Send</Button>
-            </Box>
-        </Grid2>
-
-      </div>
+      </Box>
     </div>
-  )
+  );
 }
