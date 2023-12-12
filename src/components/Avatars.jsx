501b864bdbffbf0569715a07cc736cb0f7b686dc
Nissim Amsallem <nissimamsallem@gmail.com>
2023-12-12 16:52:54 +0200

    Added an option to open a new room and see which users are online

--- src/components/Avatars.jsx
+++ src/components/Avatars.jsx
@@ -3,17 +3,54 @@ import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import { Box } from "@mui/material";
 import { Button } from "react-scroll";
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
+
 
 export default function ImageAvatars(props) {
 
     
     const socket = props.socket
     
-    const newJoinRoom = (object) => {
+    const joinRoom = (object) => {
         socket.emit("join_room", object.roomID);
         props.room(object.roomID)
         console.log(object.roomID)
     }
+    // const messageTo = (object) => {
+    //   socket.emit("join_room", object.roomID);
+    //   props.room(object.roomID)
+    //   console.log(object.roomID)
+    // }
     
 
     return (
@@ -48,13 +85,21 @@ export default function ImageAvatars(props) {
         }}
       > {props.className === "users"?
             props.users.map((object)=>
-                <Avatar alt={object.userName} key={object.nameID}/>):
+              <Button key={object.nameID} onClick={()=>messageTo(object)}>
+                <StyledBadge
+                  overlap="circular"
+                  anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                  variant={object.status==="connect"?"dot":"standart"}>
+                  <Avatar alt={object.userName}/>
+                </StyledBadge>
+                </Button>):
             
             props.rooms.map((object,index) => 
-                <Button index={index} key={object.roomID} onClick={()=>newJoinRoom(object)}>
+                <Button index={index} key={object.roomID} onClick={()=>joinRoom(object)}>
                     <Avatar alt={object.roomName}/>
                 </Button>)}
         {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" /> */}
+
 
       </Stack>
   );
---
+++ src/components/NewRoom.jsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function FormDialog(props) {
+    const socket = props.socket;
+    const modelRoom = Object.keys(props.modelRoom);
+    const [open, setOpen] = React.useState(false);
+    const [roomData,setRoomData] = React.useState({})
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleFillRoom = (event) =>{
+        const label = event.target.name;
+        const value = event.target.value;
+        setRoomData((prevRoomData) => ({ ...prevRoomData, [label]: value }));
+    }
+
+    const handleNewRoom = (e) => {
+        e.preventDefault()
+
+        console.log("get to server",roomData);
+        socket.emit("createRoom", (roomData), (response) => {
+            console.log(response.status);
+            setRoomData({})
+        });
+    };
+
+    return (
+        <React.Fragment>
+            <Button
+                sx={{ width: "12vh", marginTop: "20px" }}
+                variant="outlined"
+                onClick={handleClickOpen}>
+                Oepn New Room
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Welcome</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        In order to open a new room you must fill in the following details.
+                    </DialogContentText>
+
+                    {modelRoom.map((object, index) => (
+                        <TextField
+                            index={index}
+                            key={index}
+                            autoFocus
+                            margin="dense"
+                            className="openRoom"
+                            id={modelRoom[index]}
+                            label={modelRoom[index]}
+                            name={modelRoom[index]}
+                            variant="standard"
+                            onChange={handleFillRoom}
+                        />
+                    ))}
+                </DialogContent>
+                <DialogActions>
+                    <Button sx={{ bgcolor: "#31213E" }} onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button
+                        sx={{ bgcolor: "#31213E" }}
+                        onClick={() => {
+                            handleClose(), handleNewRoom(e);
+                        }}
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
+}
